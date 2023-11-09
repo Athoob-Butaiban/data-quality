9c056f1e2031b536b535186606d8184a8414931f
athouba99 <athooba99@gmail.com>
2023-11-09 12:14:32 +0300

    made comparison between the original dataframe and the trans data

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # data-quality
-Testing out 'ydata-profiling' library to preform reports of the dataset and add conditions to it. dataset used: https://www.kaggle.com/datasets/sakshigoyal7/credit-card-customers/
+
+Testing out 'ydata-profiling' library to preform reports about the data quality of the dataset and add conditions to it.
+Dataset used: https://www.kaggle.com/datasets/sakshigoyal7/credit-card-customers/
+
+Documintation of ydata-profiling:
+https://github.com/ydataai/ydata-profiling
+https://ydata.ai/resources/advanced-data-visualisation-with-pandas-profiling
--- main.py
+++ main.py
@@ -8,40 +8,58 @@ import yaml
 import sys
 
 
-
 # encoding
 if sys.stdout.encoding != 'utf-8':
     sys.stdout.reconfigure(encoding='utf-8')
 
-# congigs from yaml 
-with open('C:/Users/athoo/PIFSS/data-quality/config/rule.yaml','r') as rule:
+# congigs from yaml
+with open('C:/Users/athoo/PIFSS/data-quality/config/rule.yaml', 'r') as rule:
     custom_config = yaml.safe_load(rule)
 
 
-
-# loading dataset 
+# loading dataset
 df = pd.read_csv(r'C:/Users/athoo/PIFSS/data-quality/inputs/BankChurners.csv')
 print(df.columns)
 
-# Apply custom validation rules based on the rule.yaml file
+# this part making the data profiling to the original dataframe
+
+
+# make profile report for the original df
+# the only allowed params in ProfileReport are df name and title of the html page
+original_report = ProfileReport(df, title="Profiling Report")
+original_report.to_widgets()  # this is mostly used for jupetr note
+original_report.to_notebook_iframe()
+
+# generate the report of the original df
+original_report.to_file(
+    r'C:/Users/athoo/PIFSS/data-quality/outputs/profile-report.html')
+
+
+# this part is for the transformed dataframe( appling the rules to it)
+
+
+# make the comparisone between the original and the transformed df
+# 1- make a copy of the original df
+df_trans = df.copy()
+
+# applying custom validation rules based on rule.yaml file
+# 2- appy the rules to the df_trans
 for rule_entry in custom_config['rule']:
     column = rule_entry['column']
     if 'range' in rule_entry:
         min_range, max_range = rule_entry['range']
-        df = df[(df[column] >= min_range) & (df[column] <= max_range)]
+        df_trans = df_trans[(df_trans[column] >= min_range)
+                            & (df_trans[column] <= max_range)]
     if 'string_length' in rule_entry:
         min_length, max_length = rule_entry['string_length']
-        df = df[df[column].str.len().between(min_length, max_length)]
-    
+        df_trans = df_trans[df_trans[column].str.len().between(
+            min_length, max_length)]
 
-
-# profile report
-profile = ProfileReport(df, title= "Profiling Report") # this function doesn't accept other params than the used 
-profile.to_widgets()
-profile.to_notebook_iframe()
-
-# generate the report + pass custom_config to Jinja2 template
-profile.to_file(r'C:/Users/athoo/PIFSS/data-quality/outputs/profile-report.html')
+# generate the html report for the df_trans
+trans_report = ProfileReport(df_trans, title='Transformed Data')
+comparison_report = original_report.compare(trans_report)
+comparison_report.to_file(
+    r'C:/Users/athoo/PIFSS/data-quality/outputs/original-vs-trans.html')
 
 
 # Specify the directory where the HTML report is located
@@ -54,8 +72,7 @@ Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 
 # Open the web browser to view the report
-webbrowser.open(f'http://localhost:8000/outputs/profile-report.html')
-
+webbrowser.open(f'http://localhost:8000/outputs/original-vs-trans.html')
 
 # Start the local web server
 print(f"Serving at port 8000")
